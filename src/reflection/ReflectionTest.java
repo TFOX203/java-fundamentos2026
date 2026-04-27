ec7e9b74929ed422a110995f1153b37332acc9f1
TFOX203 <pesasimeran87@gmail.com>
2026-04-27 13:03:54 +0200

    reflection

---
+++ src/reflection/ReflectionTest.java
@@ -0,0 +1,13 @@
+package reflection;
+
+import oopmodeling.addressbook.AddressBook;
+
+public class ReflectionTest {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		// to load
+		Class<AddressBook> cls = AddressBook.class;
+		Class<AddressBook> cls2 = (Class<AddressBook>) new AddressBook().getClass();
+		Class<?> cls3 = Class.forName("oopmodeling.addressbook.AddressBook");
+	}
+}
