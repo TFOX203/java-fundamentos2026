a3258205fff2b36ff82e4688b1badc15ad225672
TFOX203 <pesasimeran87@gmail.com>
2026-04-28 12:42:48 +0200

    tarea

--- src/reflection/ReflectionTest.java
+++ src/reflection/ReflectionTest.java
@@ -1,13 +1,53 @@
 package reflection;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+
 import oopmodeling.addressbook.AddressBook;
 
 public class ReflectionTest {
 
 	public static void main(String[] args) throws ClassNotFoundException {
 		// to load
-		Class<AddressBook> cls = AddressBook.class;
+		Class<AddressBook> cls1 = AddressBook.class;
 		Class<AddressBook> cls2 = (Class<AddressBook>) new AddressBook().getClass();
 		Class<?> cls3 = Class.forName("oopmodeling.addressbook.AddressBook");
+		
+		inspectClass(cls1);
+	}
+
+	private static void inspectClass(Class<AddressBook> cls) {
+		
+		cls.getDeclaredConstructors();
+
+		Method[] ms = cls.getDeclaredMethods();
+		for(int i = 0; i < ms.length; i++) {
+
+			System.out.print("method name is " + ms[i].getName());
+			System.out.print(" | parameters: ");
+
+			Parameter[] params = ms[i].getParameters();
+
+			for(int j = 0; j < params.length; j++) {
+				System.out.print(params[j].getType().getSimpleName() + " " + params[j].getName());
+
+				if(j < params.length - 1) {
+					System.out.print(", ");
+				}
+			}
+
+			System.out.println();
+		}
+
+		for(Field field : cls.getDeclaredFields()) {
+			System.out.println("field name is " + field.getName());
+			System.out.println("field type is " + field.getType().getSimpleName());
+		}
+		
+		for (Annotation annotation : cls.getAnnotations()) {
+			System.out.println("annotation is " + annotation.annotationType().getSimpleName());
+		}
 	}
 }
